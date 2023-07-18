1ae536292174cf6fcd1999151d68ca4d205a64c9
Marco Hanquier <marco.hanquier@hotmail.fr>
2023-07-18 14:57:34 +0200

    created project angular skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,26 @@ import { DemoPageComponent } from './pages/demo/demo.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { DemoPipe } from './pipes/demo.pipe';
 import { DemoDirective } from './directives/demo.directive';
+import { OverviewComponent } from './components/overview/overview.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { BuildingsComponent } from './components/buildings/buildings.component';
+import { ResearchComponent } from './components/research/research.component';
+import { FleetComponent } from './components/fleet/fleet.component';
+import { AlliancesComponent } from './components/alliances/alliances.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AccountComponent } from './components/account/account.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { DefenseComponent } from './components/defense/defense.component';
+import { MapComponent } from './components/map/map.component';
+import { MenubuttonComponent } from './components/menubutton/menubutton.component';
+import { PlayerComponent } from './components/player/player.component';
+import { MessageComponent } from './components/message/message.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoPageComponent, DemoComponent, DemoPipe, DemoDirective],
+  declarations: [AppComponent, DemoPageComponent, DemoComponent, DemoPipe, DemoDirective, OverviewComponent, ResourcesComponent, BuildingsComponent, ResearchComponent, FleetComponent, AlliancesComponent, LoginComponent, RegisterComponent, AccountComponent, RankingComponent, AdminComponent, DefenseComponent, MapComponent, MenubuttonComponent, PlayerComponent, MessageComponent, PopupComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/defense/defense.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defense',
+  templateUrl: './defense.component.html',
+  styleUrls: ['./defense.component.scss']
+})
+export class DefenseComponent {
+
+}
