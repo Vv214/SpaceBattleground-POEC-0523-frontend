743270235c3c195d53bc865b32f55e0c1bcdcf10
adadofr <thioff1@gmail.com>
2023-07-18 17:20:03 +0200

    branch init

--- src/app/components/alliances/alliances.component.ts
+++ src/app/components/alliances/alliances.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-alliances',
   templateUrl: './alliances.component.html',
-  styleUrls: ['./alliances.component.scss']
+  styleUrls: ['./alliances.component.scss'],
 })
-export class AlliancesComponent {
-
-}
+export class AlliancesComponent {}
