8772924ab1e446e223d5808fd2526623d6155ce8
ROYPIDO <roypido@gmail.com>
2023-07-24 11:46:29 +0200

    une petite modification pour eviter les erreurs sur alliance.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,10 +12,10 @@ import { DemoComponent } from './components/demo/demo.component';
 import { DemoPipe } from './pipes/demo.pipe';
 import { DemoDirective } from './directives/demo.directive';
 import { OverviewComponent } from './components/overview/overview.component';
-import { ResourcesComponent } from './components/resources/resources.component';
+import { ResourcesComponent, Upgrade } from './components/resources/resources.component';
 import { BuildingsComponent } from './components/buildings/buildings.component';
 import { ResearchComponent } from './components/research/research.component';
-import { FleetComponent } from './components/fleet/fleet.component';
+import { FleetComponent, modifyFleet, shipDetail } from './components/fleet/fleet.component';
 import { AlliancesComponent } from './components/alliances/alliances.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -26,10 +28,42 @@ import { MenubuttonComponent } from './components/menubutton/menubutton.componen
 import { PlayerComponent } from './components/player/player.component';
 import { MessageComponent } from './components/message/message.component';
 import { PopupComponent } from './components/popup/popup.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, DemoPageComponent, DemoComponent, DemoPipe, DemoDirective, OverviewComponent, ResourcesComponent, BuildingsComponent, ResearchComponent, FleetComponent, AlliancesComponent, LoginComponent, RegisterComponent, AccountComponent, RankingComponent, AdminComponent, DefenseComponent, MapComponent, MenubuttonComponent, PlayerComponent, MessageComponent, PopupComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    DemoPageComponent,
+    DemoComponent,
+    DemoPipe,
+    DemoDirective,
+    OverviewComponent,
+    ResourcesComponent,
+    BuildingsComponent,
+    ResearchComponent,
+    FleetComponent,
+    AlliancesComponent,
+    LoginComponent,
+    RegisterComponent,
+    AccountComponent,
+    RankingComponent,
+    AdminComponent,
+    DefenseComponent,
+    MapComponent,
+    MenubuttonComponent,
+    PlayerComponent,
+    MessageComponent,
+    PopupComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    Upgrade,
+    modifyFleet,
+    shipDetail,
+  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatDialogModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(private router: Router) {}
 
+  onSubmit() {
+    // création compte
+    this.router.navigate(['/', 'overview']);
+  }
+
+  login() {
+    // redirection login
+    this.router.navigate(['/', 'login']);
+  }
 }
