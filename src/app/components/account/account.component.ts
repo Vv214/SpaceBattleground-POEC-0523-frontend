cc99a6d5f82a0c9ce39895abfd2d7f6a4ff1dafd
Vv214 <vrf.lbc@gmail.com>
2023-07-31 10:00:44 +0200

    essais d auth avec token part 1

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { PlayerService } from '../../services/player.service';
 
 @Component({
   selector: 'app-account',
@@ -7,7 +8,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, public playerService: PlayerService) {}
+
+  public nickname = localStorage.getItem('nickname');
+  public toto() {
+    console.log(this.nickname);
+  }
 
   toMessages() {
     this.router.navigate(['/', 'messages']);
@@ -19,5 +25,8 @@ export class AccountComponent {
 
   logout() {
     this.router.navigate(['/', 'home']);
+  }
+
+  onInit() {
   }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-register',
@@ -17,22 +18,28 @@ export class RegisterComponent {
     confirmPassword: ['', Validators.required],
   });
 
+  public nicknameLocal!: string;
+
   constructor(private router: Router, private fb: FormBuilder) {};
 
+  alreadyInBase = true;
   onSubmit() {
     // création compte
     this.addUser();
-    // console.log(this.registerForm.value.nickname + "Toto");
+    this.updateLocalStorage();
+    console.log(this.registerForm.value.nickname + "Toto");
     this.router.navigate(['/', 'overview']);
   }
 
   login() {
     // redirection login
+    this.updateLocalStorage();
     this.router.navigate(['/', 'login']);
+
   }
 
   addUser() {
-    console.log(this.registerForm.value);
+    // console.log(this.registerForm.value);
     fetch("http://localhost:8080/register", {
       method: "post",
       headers: {
@@ -41,8 +48,13 @@ export class RegisterComponent {
       },
       //make sure to serialize your JSON body
       body: JSON.stringify(this.registerForm.value)
-    }).then(response => console.log(response));
+    }).then(response => console.log(response + " response"));
   };
-}
 
+  updateLocalStorage(): void {
+    console.log("dans la méthode update local storage", localStorage.getItem("nickname"));
+    this.nicknameLocal = this.registerForm.value.nickname || " ";
+    console.log("dans la méthode update local storage après la maj", this.nicknameLocal);
+  }
+}
 
