1c1d3ff2c7283f53fc24aeb577abe9262366cf23
Marco Hanquier <marco.hanquier@hotmail.fr>
2023-08-04 16:43:57 +0200

    test link back

--- src/app/services/build.service.ts
+++ src/app/services/build.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
 
+export interface Buildings {
+  data: {
+    ironMine: Building;
+    diamondMine: Building;
+    hydrogeneMine: Building;
+    energyMine: Building;
+
+    laboratory: Building;
+    robotFactory: Building;
+    shipyard: Building;
+    drill: Building;
+  };
+}
+
+export interface Building {
+  name: string;
+  type: string;
+  description: string;
+  level: number;
+  ironPrice: number;
+  diamondPrice: number;
+  hydrogenePrice: number;
+  energyPrice: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class BuildService {
   constructor() {}
+
+  checkBuildingInfo(token: string) {
+    return fetch('http://localhost:8080/building', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'x-token': token,
+      },
+    });
+  }
 
   buildBuilding(token: string) {
     return fetch('http://localhost:8080/buildings/{name}', {
