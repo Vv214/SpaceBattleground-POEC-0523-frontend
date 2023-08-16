4bf013c0eca1e4f2326cb561a7ab1e730ebc9711
Alzuran <marco.hanquier@hotmail.fr>
2023-08-16 17:21:36 +0200

    added ship build

--- src/app/components/shipyard/shipyard.component.ts
+++ src/app/components/shipyard/shipyard.component.ts
@@ -2,6 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { ShipService } from 'src/app/services/ship.service';
+import { BuildService } from 'src/app/services/build.service';
+import { MethodService } from 'src/app/services/method.service';
+import { NavbarService } from 'src/app/services/navbar.service';
+import { errorMessage } from '../buildings/buildings.component';
+
+
+export interface Ressources {
+  data: {
+    iron: Ressource;
+    diamond: Ressource;
+    hydrogene: Ressource;
+    energy: Ressource;
+  };
+}
+
+export interface Ressource {
+  quantity: number;
+  maxStock: number;
+}
 
 export interface Ships {
   data: {
@@ -100,8 +119,25 @@ export class ShipyardComponent implements OnInit {
     this.shipService.shipEnergyPrice = ships.data[shipName].energyPrice;
 
     this.shipService.shipNameSrc = shipName;
+    
+    if (shipName == 'lightShip') {
+      this.shipService.shipQuantity = this.shipService.lightShipQuantity;
+    } else if (shipName == 'mediumShip') {
+      this.shipService.shipQuantity = this.shipService.mediumShipQuantity;
+    } else if (shipName == 'heavyShip') {
+      this.shipService.shipQuantity = this.shipService.heavyShipQuantity;
+    } else if (shipName == 'scoutShip') {
+      this.shipService.shipQuantity = this.shipService.scoutShipQuantity;
+    } else if (shipName == 'cargoShip') {
+      this.shipService.shipQuantity = this.shipService.cargoShipQuantity;
+    } else if (shipName == 'heavyCargoShip') {
+      this.shipService.shipQuantity = this.shipService.heavyCargoShipQuantity;
+    } else if (shipName == 'recyclerShip') {
+      this.shipService.shipQuantity = this.shipService.recyclerShipQuantity;
+    } else if (shipName == 'colonisateur') {
+      this.shipService.shipQuantity = this.shipService.colonisateurQuantity;
+    }
 
-    console.log();
 
     const dialogRef = this.dialog.open(shipDetail);
     dialogRef.afterClosed().subscribe((result) => {
@@ -125,14 +161,14 @@ export class ShipyardComponent implements OnInit {
     this.getShipInfo(this.token);
     let ships: Ships = JSON.parse(localStorage.getItem('ships') ?? '');
 
-    this.lightShipQuantity = ships.data.lightShip.quantity;
-    this.mediumShipQuantity = ships.data.mediumShip.quantity;
-    this.heavyShipQuantity = ships.data.heavyShip.quantity;
-    this.scoutShipQuantity = ships.data.scoutShip.quantity;
-    this.cargoShipQuantity = ships.data.cargoShip.quantity;
-    this.heavyCargoShipQuantity = ships.data.heavyCargoShip.quantity;
-    this.recyclerShipQuantity = ships.data.recyclerShip.quantity;
-    this.colonisateurQuantity = ships.data.colonisateur.quantity;
+    this.shipService.lightShipQuantity = ships.data.lightShip.quantity;
+    this.shipService.mediumShipQuantity = ships.data.mediumShip.quantity;
+    this.shipService.heavyShipQuantity = ships.data.heavyShip.quantity;
+    this.shipService.scoutShipQuantity = ships.data.scoutShip.quantity;
+    this.shipService.cargoShipQuantity = ships.data.cargoShip.quantity;
+    this.shipService.heavyCargoShipQuantity = ships.data.heavyCargoShip.quantity;
+    this.shipService.recyclerShipQuantity = ships.data.recyclerShip.quantity;
+    this.shipService.colonisateurQuantity = ships.data.colonisateur.quantity;
   }
 }
 
@@ -195,14 +231,14 @@ export class shipDetail {
     this.getShipInfo(this.token);
     let ships: Ships = JSON.parse(localStorage.getItem('ships') ?? '');
 
-    this.lightShipQuantity = ships.data.lightShip.quantity;
-    this.mediumShipQuantity = ships.data.mediumShip.quantity;
-    this.heavyShipQuantity = ships.data.heavyShip.quantity;
-    this.scoutShipQuantity = ships.data.scoutShip.quantity;
-    this.cargoShipQuantity = ships.data.cargoShip.quantity;
-    this.heavyCargoShipQuantity = ships.data.heavyCargoShip.quantity;
-    this.recyclerShipQuantity = ships.data.recyclerShip.quantity;
-    this.colonisateurQuantity = ships.data.colonisateur.quantity;
+    // this.lightShipQuantity = ships.data.lightShip.quantity;
+    // this.mediumShipQuantity = ships.data.mediumShip.quantity;
+    // this.heavyShipQuantity = ships.data.heavyShip.quantity;
+    // this.scoutShipQuantity = ships.data.scoutShip.quantity;
+    // this.cargoShipQuantity = ships.data.cargoShip.quantity;
+    // this.heavyCargoShipQuantity = ships.data.heavyCargoShip.quantity;
+    // this.recyclerShipQuantity = ships.data.recyclerShip.quantity;
+    // this.colonisateurQuantity = ships.data.colonisateur.quantity;
 
     this.shipName = this.shipService.shipName.toString();
     this.shipType = this.shipService.shipType.toString();
@@ -225,6 +261,167 @@ export class shipDetail {
   templateUrl: 'shipBuild.html',
   styleUrls: ['shipBuild.scss'],
 })
-export class shipBuild {
-  constructor(public dialog: MatDialog) {}
+export class shipBuild implements OnInit{
+
+  constructor(
+    public dialog: MatDialog,
+    private buildService: BuildService,
+    private methodService: MethodService,
+    private navbarService:NavbarService,
+    private shipService:ShipService
+  ) {}
+  public shipName!: string;
+  public shipNameSrc!: string;
+  public shipType!: string;
+  public shipSpeed!: number;
+  public shipPv!: number;
+  public shipDamage!: number;
+  public shipFuel!: number;
+  // public shipDescription!: string;       à ajouter
+  public shipCapacity!: number;
+  public shipQuantity!: number;
+
+  public shipIronPrice!: number;
+  public shipDiamondPrice!: number;
+  public shipHydrogenePrice!: number;
+  public shipEnergyPrice!: number;
+
+  public token!: string;
+  public ironPlayer!: number;
+  public diamondPlayer!: number;
+  public hydrogenePlayer!: number;
+  public energyPlayer!: number;
+
+  openErrorMessage() {
+    const dialogRef = this.dialog.open(errorMessage);
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  buildShip(
+    token: string,
+    shipName: string,
+    shipQuantity: number,
+    shipIronPrice: number,
+    shipDiamondPrice: number,
+    shipEnergyPrice: number,
+    shipHydrogenePrice: number,
+    ironPlayer: number,
+    diamondPlayer: number,
+    hydrogenePlayer: number,
+    energyPlayer: number
+  ) {
+    let ressourcesPlayer: Array<number> = [4];
+    ressourcesPlayer[0] = ironPlayer;
+    ressourcesPlayer[1] = diamondPlayer;
+    ressourcesPlayer[2] = hydrogenePlayer;
+    ressourcesPlayer[3] = energyPlayer;
+
+    let canBuild = this.methodService.haveEnoughRessources(
+      shipIronPrice,
+      shipDiamondPrice,
+      shipEnergyPrice,
+      shipHydrogenePrice,
+      ironPlayer,
+      diamondPlayer,
+      hydrogenePlayer,
+      energyPlayer
+    );
+
+
+  // Nouvelle version, à checker
+  if (canBuild) {
+    let ships: Ships = JSON.parse(localStorage.getItem('ships') ?? '');
+
+    this.methodService
+      .updateStockPlayer(
+        token,
+        shipIronPrice,
+        shipDiamondPrice,
+        shipEnergyPrice,
+        shipHydrogenePrice,
+        ironPlayer,
+        diamondPlayer,
+        hydrogenePlayer,
+        energyPlayer
+      )
+      .then(() => {
+        shipQuantity++;
+        this.methodService.changeIsBuild(token, shipName, shipQuantity).then((response) => {
+          if (response.status === 200) {
+            response.json().then((body) => {
+              if (body.quantity !== 0) {
+                if (shipName === 'Chasseur léger') {
+                  this.shipService.lightShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Chasseur lourd') {
+                  this.shipService.mediumShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Destroyer') {
+                  this.shipService.heavyShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Eclaireur') {
+                  this.shipService.scoutShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Transporteur léger') {
+                  this.shipService.cargoShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Transporteur lourd') {
+                  this.shipService.heavyCargoShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Récolteur') {
+                  this.shipService.recyclerShipQuantity = shipQuantity;
+                }
+                if (shipName === 'Colonisateur') {
+                  this.shipService.colonisateurQuantity = shipQuantity;
+                }
+                
+                this.checkQuantityRessource(token);
+              } 
+             
+            });
+          }
+        });
+      });
+  } else {
+    this.buildService.eMessage = 'ressources';
+    this.openErrorMessage();
+  }
+}
+
+  ngOnInit() {
+    this.token = localStorage.getItem('x-token') ?? '';
+    this.shipName = this.shipService.shipName.toString();
+    this.shipType = this.shipService.shipType.toString();
+    this.shipQuantity = this.shipService.shipQuantity;
+
+
+
+    // this.shipDescription = this.buildService.buildingDescription.toString();
+    this.shipIronPrice = this.shipService.shipIronPrice;
+    this.shipDiamondPrice = this.shipService.shipDiamondPrice;
+    this.shipHydrogenePrice = this.shipService.shipHydrogenPrice;
+    this.shipEnergyPrice = this.shipService.shipEnergyPrice;
+    this.shipNameSrc = this.shipService.shipNameSrc;
+    let ressources = JSON.parse(localStorage.getItem('ressources') ?? '');
+    this.ironPlayer = ressources.data.iron.quantity;
+    this.diamondPlayer = ressources.data.diamond.quantity;
+    this.hydrogenePlayer = ressources.data.hydrogene.quantity;
+    this.energyPlayer = ressources.data.energy.quantity;
+  }
+
+  checkQuantityRessource(token: string) {
+    this.navbarService.checkQuantityRessource(token).then((response) => {
+      if (response.status === 200) {
+        response.json().then((body: Ressources) => {
+          localStorage.setItem('ressources', JSON.stringify(body));
+        });
+      }
+    });
+  }
+
+  upgradeBuilding() {
+    // incrémenter level, coeff prod et ressources requises du batiment en cours
+  }
 }
